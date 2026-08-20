58c8379ed75ae9a0fadf46184a4ec73284ef3d09
BrenoOliveiradev <brenooliveira.dev@hotmail.com>
2026-08-20 00:08:02 -0300

    Validação de dados

--- src/controllers/users/CreateUserController.ts
+++ src/controllers/users/CreateUserController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 import CreateUserService from "../../services/users/CreateUserService.js";
+import { createUserSchema } from "../../schemas/users/createUser.schema.js";
 class CreateUserController {
     async handle(req: Request, res: Response) {
-        
+        const isValid = createUserSchema.safeParse(req.body);
+
+        if (!isValid.success) {
+            return res.status(400).json({ message: "Erro de validação" });
+        }
         const input = req.body;
 
         const service = new CreateUserService();
